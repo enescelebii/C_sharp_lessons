b71ce1d8b29965f75ee4efa16f22a9d1bbbfdf23
Enes <ereseres1579@gmail.com>
2024-09-25 18:56:55 +0300

    Proje dosyası ekle.

---
+++ Week_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    internal class Program
+    {
+        class Bread
+        {
+            public int counter;
+        }
+
+        static class Butter
+        {
+            public static int counter;
+        }
+
+        static void Main(string[] args)
+        {
+            Bread a = new Bread();
+            a.counter = 1;
+            Console.WriteLine(a.counter);
+            Butter.counter = 8;
+        }
+    }
+}
