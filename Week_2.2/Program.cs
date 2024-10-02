7fcc1e6d06b46e2a100122b059d91691ab45aad6
Enes <ereseres1579@gmail.com>
2024-10-02 18:31:38 +0300

    School

---
+++ Week_2.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 34, 72, 13, 44, 25, 30, 10 };
+            int[] temp = list;
+            Console.WriteLine("ordinal Array:");
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();
+            Array.Reverse(list);
+            Console.WriteLine("Reversed Array: ");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Array.Sort(list);
+            Console.WriteLine("Sorted array: ");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            int i;
+            for (i = 0; i < n.Length; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            for (i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine("Element [{0}] - {1}", i, n[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
