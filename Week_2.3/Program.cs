4d63cac2ecb5669104637906de8cbb6190806679
Enes <ereseres1579@gmail.com>
2024-10-05 14:27:33 +0300

    School

---
+++ Week_2.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list_1 = new ArrayList();
+
+            list_1.Add(1);
+            list_1.Add("*");
+            list_1.Add(true);
+            list_1.Add(null);
+            list_1.Add(1.5);
+
+            int[] arr = { 100, 200, 300, 400 };
+
+            Queue myQ = new Queue();
+            myQ.Enqueue("Hello");
+            myQ.Enqueue("World!");
+
+            list_1.AddRange(arr);
+            list_1.AddRange(myQ);
+
+
+            foreach (var item in list_1)
+                Console.Write(item + ", ");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week_2.4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2._4
+{
+    internal class Program
+    {
+        public void swap(ref int x,ref int y)
+        {
+
+            x = x - y;
+            y = y + x;
+            x = y - x;
+        }
+        static void Main(string[] args)
+        {
+            Program n = new Program();
+
+            int b = 100;
+            int a = 200;
+
+            //arkadaşlar
+
+            Console.WriteLine("Before swap, value of a : {0}", a);
+            Console.WriteLine("Before swap, value of b : {0}", b);
+
+            n.swap(ref a,ref b);
+
+            Console.WriteLine("After swap, value of a : {0}", a);
+            Console.WriteLine("After swap, value of b : {0}", b);
+            Console.ReadLine();
+        }
+    }
+}
